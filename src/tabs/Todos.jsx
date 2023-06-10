98d0453373efafea525715e106d4a4eec8864bee
Liza <lizapikovskaya@gmail.com>
2023-06-10 12:36:30 +0300

    task-2

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -3,7 +3,53 @@ import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
 export class Todos extends Component {
+  state = {
+    todos: [{ id: 1, text: 'baby' }],
+  };
+
+  componentDidMount() {
+    const data = localStorage.getItem('todo')
+    if(data !== null) {
+      this.setState({todos: JSON.parse(data)})
+    }
+  }
+  componentDidUpdate(_, prevState) {
+    const {todos} = this.state
+    if(prevState.todos !== todos) {
+      localStorage.setItem('todo', JSON.stringify(todos))
+    }
+  }
+  addToDo = text => {
+    const todo = {
+      text,
+      id: nanoid(),
+    };
+    console.log(text);
+    this.setState(prevState => ({
+      todos: [...prevState.todos, todo],
+    }));
+  };
+
+  deleteToDo = (id) => {
+this.setState((prevState)=> ({
+  todos: prevState.todos.filter((item) => item.id !== id)
+}))
+  }
   render() {
-    return <Text>Todos</Text>;
+    const { todos } = this.state;
+    return (
+      <>
+        <SearchForm onSubmit={this.addToDo} />
+        <Grid>
+          {todos.map(({ text, id }, idx) => {
+            return (
+              <GridItem key={id}>
+                <Todo text={text} count={idx + 1} onDelete={this.deleteToDo} id={id}/>
+              </GridItem>
+            );
+          })}
+        </Grid>
+      </>
+    );
   }
 }
